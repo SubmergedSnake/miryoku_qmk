2431804490325278f9baea9e42be367ddf6edb74
Joonas Heikkinen <solarshadow.jh@gmail.com>
2026-08-13 00:57:54 +0300

    Update math.py
    fix invalid syntax

--- lib/python/qmk/math.py
+++ lib/python/qmk/math.py
@@ -23,7 +23,7 @@ def compute(expr):
 
 
 def _eval(node):
-    if isinstance(node, ast.Constant:  # <number>
+    if isinstance(node, ast.Constant):  # <number>
         return node.value
     elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
         return operators[type(node.op)](_eval(node.left), _eval(node.right))
